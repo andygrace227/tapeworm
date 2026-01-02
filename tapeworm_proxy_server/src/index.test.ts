6997b04e770f6ec4608851b7e8583ff05950ca58
Andy Grace <andygrace227@gmail.com>
2026-01-01 16:57:46 -0800

    Proxy Server Unit Tests

---
+++ tapeworm_proxy_server/src/index.test.ts
@@ -0,0 +1,65 @@
+import { Model } from "@atgs/tapeworm";
+import {ProcessLocalRateLimiter, TapewormConnectionInformationGranularity, TapewormConnectionInformation} from "./index"
+class FakeModel extends Model {
+  requests: any[] = [];
+  private responses: any[];
+
+  constructor(responses: any[]) {
+    super();
+    this.responses = [...responses];
+  }
+
+  async invoke(request: any) {
+    this.requests.push(request);
+    return this.responses.shift() as any;
+  }
+}
+
+
+describe("Process Local Limiter works as expected", () => {
+
+    it("rejects when user sends too many requests", () => {
+        let rateLimiter = new ProcessLocalRateLimiter();
+        rateLimiter.granularity = TapewormConnectionInformationGranularity.IP;
+        rateLimiter.grantedRequestsPerSecondReciprocal = 1;
+        let connectionInfo = new TapewormConnectionInformation();
+        connectionInfo.ip = "fakeIP";
+
+        expect(rateLimiter.attemptGrant(connectionInfo)).toBe(true);
+        expect(rateLimiter.attemptGrant(connectionInfo)).toBe(false);
+    });
+    
+    it("does not reject when user sends rate limited requests", async () => {
+        let rateLimiter = new ProcessLocalRateLimiter();
+        rateLimiter.granularity = TapewormConnectionInformationGranularity.IP;
+        rateLimiter.grantedRequestsPerSecondReciprocal = 1;
+        let connectionInfo = new TapewormConnectionInformation();
+        connectionInfo.ip = "fakeIP";
+
+        expect(rateLimiter.attemptGrant(connectionInfo)).toBe(true);
+        await new Promise((r) => setTimeout(r, 1001));
+        expect(rateLimiter.attemptGrant(connectionInfo)).toBe(true);
+    });
+
+    it("can handle multiple users", async () => {
+        let rateLimiter = new ProcessLocalRateLimiter();
+        rateLimiter.granularity = TapewormConnectionInformationGranularity.IP;
+        rateLimiter.grantedRequestsPerSecondReciprocal = 1;
+        let connectionInfo1 = new TapewormConnectionInformation();
+        connectionInfo1.ip = "fakeIP";
+
+        let connectionInfo2 = new TapewormConnectionInformation();
+        connectionInfo2.ip = "fakeIP2";
+
+        expect(rateLimiter.attemptGrant(connectionInfo1)).toBe(true);
+        expect(rateLimiter.attemptGrant(connectionInfo2)).toBe(true);
+        
+        expect(rateLimiter.attemptGrant(connectionInfo1)).toBe(false);
+        expect(rateLimiter.attemptGrant(connectionInfo2)).toBe(false);
+        await new Promise((r) => setTimeout(r, 1001));
+        expect(rateLimiter.attemptGrant(connectionInfo1)).toBe(true);
+        expect(rateLimiter.attemptGrant(connectionInfo2)).toBe(true);
+        
+    });
+
+});
--- tapeworm_proxy_server/src/index.ts
+++ tapeworm_proxy_server/src/index.ts
@@ -1,41 +1,91 @@
 /**
  * Tapeworm Proxy Server
  * ©️ Andy Grace 2025. All Rights Reserved.
- * 
+ *
  * Tapeworm is a framework for building agents in Typescript and in JavaScript, and it runs in the browser and in Node or another server runtime.
  *
  * Tapeworm's Proxy Servers cater to the browser use case and allow you to securely provide access to an LLM of your choosing, using
  * Tapeworm's models.
- * 
+ *
  * This implementation seeks to be very dev-friendly and has these features:
  * - Rate limiting per user - allows you to securely give limited access to LLMs.
  * - Keeps your API keys secure by proxying instead of giving users direct access to your keys
- * 
+ *
  * @module
  */
 import { Model, ModelRequest } from "@atgs/tapeworm";
 
 /**
- * A reference, ready-to-go implementation of a proxy server for TapeWorm.
- * 
+ * A reference, ready-to-go implementation of a proxy server for Tapeworm.
+ *
  * This can be used with Tapeworm's TapewormProxyServer class so you can run in-browser agentic AI.
- * 
+ *
  * While this implementation of Tapeworm assumes you will be using an Express.JS server setup, it's not enforced,
  * and you can use wrapper classes to get around this assumption.
  */
 export default class TapewormProxyServer {
-    model : Model;
-    rateLimiter? : TapewormRateLimiter;
+  model: Model;
+  rateLimiter?: TapewormRateLimiter;
 
-    /**
-     * Invoke the model synchronously.
-     */
-    invokeSync(modelRequest : ModelRequest, connectionInfo: TapewormConnectionInformation) {
-        if (this.rateLimiter == undefined || this.rateLimiter.attemptGrant(connectionInfo)) {
-            return this.model.invoke(modelRequest);
-        }
-        throw new Error("Too many clients using the model right now. Please try again later.");
+  constructor(model: Model, rateLimiter: TapewormRateLimiter | undefined) {
+    this.model = model;
+    this.rateLimiter = rateLimiter;
+  }
+
+  /**
+   * Invoke the model synchronously.
+   */
+  invokeSync(
+    modelRequest: ModelRequest,
+    connectionInfo: TapewormConnectionInformation,
+  ) {
+    if (
+      this.rateLimiter == undefined ||
+      this.rateLimiter.attemptGrant(connectionInfo)
+    ) {
+      return this.model.invoke(modelRequest);
     }
+    throw new Error(
+      "Too many clients using the model right now. Please try again later.",
+    );
+  }
+
+  static builder() {
+    return new TapewormProxyServerBuilder();
+  }
+}
+
+/**
+ * The builder for Tapeworm Proxy Servers
+ */
+
+export class TapewormProxyServerBuilder {
+  _model!: Model;
+  _rateLimiter?: TapewormRateLimiter;
+
+  /**
+   * Set the model of the proxy server.
+   * @param model the model of the proxy server
+   * @returns The builder.
+   */
+  model(model : Model) : TapewormProxyServerBuilder {
+    this._model = model;
+    return this;
+  }
+
+  /**
+   * Set the rate limiter of the proxy server.
+   * @param rateLimiter the rate limiter of the proxy server
+   * @returns The builder.
+   */
+  rateLimiter(rateLimiter : TapewormRateLimiter | undefined) : TapewormProxyServerBuilder {
+    this._rateLimiter = rateLimiter;
+    return this;
+  }
+
+  build() {
+    return new TapewormProxyServer(this._model, this._rateLimiter);
+  }
 
 }
 
@@ -43,28 +93,28 @@ export default class TapewormProxyServer {
  * TapewormConnectionInformation is designed to let you use any framework for your server.
  */
 export class TapewormConnectionInformation {
-    ip!: string;
-    user!: string;
+  ip!: string;
+  user!: string;
 }
-
 
 /**
  * The Rate Limiter for Tapeworm provides an interface for rate limiting clients.
- * 
+ *
  * There is one function: `attemptGrant`.
- * 
+ *
  * THIS IS JUST A BASE CLASS. You will need to bring your own implementation for load balancing across multiple servers.
  */
 export class TapewormRateLimiter {
-
-    /**
-     * Attempt to grant the client access to the model. 
-     * @param connectionInfo a TapewormConnectionInformation object with either the user ID or the IP address.
-     * @returns a boolean. TRUE if the connection is accepted.
-     */
-    attemptGrant(connectionInfo: TapewormConnectionInformation) : boolean {
-        throw new TapewormRateLimiterNotDefinedError("attemptGrant was not defined by this implementation.");
-    }
+  /**
+   * Attempt to grant the client access to the model.
+   * @param connectionInfo a TapewormConnectionInformation object with either the user ID or the IP address.
+   * @returns a boolean. TRUE if the connection is accepted.
+   */
+  attemptGrant(connectionInfo: TapewormConnectionInformation): boolean {
+    throw new TapewormRateLimiterNotDefinedError(
+      "attemptGrant was not defined by this implementation.",
+    );
+  }
 }
 
 /**
@@ -79,7 +129,7 @@ class TapewormRateLimiterNotDefinedError extends Error {
 
 export const TapewormConnectionInformationGranularity = {
   Username: "username",
-  IP: "ip"
+  IP: "ip",
 };
 
 export type TapewormConnectionInformationGranularity =
@@ -87,41 +137,46 @@ export type TapewormConnectionInformationGranularity =
 
 /**
  * A ProcessLocalRateLimiter is a mechanism that can prevent users from sending too many requests per second.
- * 
+ *
  * It only works per server process, so if you have a fleet of servers, then you probably should not be using this.
  */
 export class ProcessLocalRateLimiter extends TapewormRateLimiter {
-    granularity!: TapewormConnectionInformationGranularity;
-    grantedRequestsPerSecondReciprocal!: number;
-    clients!: any;
+  granularity!: TapewormConnectionInformationGranularity;
+  grantedRequestsPerSecondReciprocal!: number;
+  clients!: any;
 
-    constructor(){
-        super();
+  constructor() {
+    super();
+        this.clients = {};
+
+  }
+
+  /**
+   * Attempt to grant the client access to the model.
+   */
+  attemptGrant(connectionInfo: TapewormConnectionInformation): boolean {
+    let identifier = connectionInfo.ip;
+    switch (this.granularity) {
+      case TapewormConnectionInformationGranularity.Username:
+        identifier = connectionInfo.user;
+        break;
+      default:
+        identifier = connectionInfo.ip;
+        break;
     }
 
-    /**
-     * Attempt to grant the client access to the model. 
-     */
-    attemptGrant(connectionInfo: TapewormConnectionInformation) : boolean {
-        let identifier = connectionInfo.ip;
-        switch (this.granularity) {
-            case TapewormConnectionInformationGranularity.Username:
-                identifier = connectionInfo.user;
-            break;
-            default:
-                identifier = connectionInfo.ip;
-            break;
-        }
-
-        if (identifier in this.clients == false) {
-            this.clients[identifier] = 0;
-        }
-
-        if (this.grantedRequestsPerSecondReciprocal + this.clients[identifier] > Date.now()) {
-            this.clients[identifier] = Date.now();
-            return true;
-        }
-
-        return false;
+    if (identifier in this.clients == false) {
+      this.clients[identifier] = 0;
     }
+
+    if (
+      this.grantedRequestsPerSecondReciprocal + this.clients[identifier] <
+      Date.now()
+    ) {
+      this.clients[identifier] = Date.now();
+      return true;
+    }
+
+    return false;
+  }
 }
